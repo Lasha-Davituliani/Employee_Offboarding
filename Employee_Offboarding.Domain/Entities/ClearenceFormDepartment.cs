bae737e0e8723b5c599d3e50428994024059719e
Your Name <your.email@example.com>
2026-02-23 16:33:23 +0400

    Add clearance form entities and repository abstractions

---
+++ Employee_Offboarding.Domain/Entities/ClearenceFormDepartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Offboarding.Domain.Entities
+{
+    public class ClearenceFormDepartment
+    {
+        public int ClearenceFormId { get; set; }
+        public ClearenceForm ClearenceForm { get; set; } = null!;
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; } = null!;
+    }
+}
