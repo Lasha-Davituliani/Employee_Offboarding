6d97e882ba76c511e8df1bea665b4cca07ca914b
Your Name <your.email@example.com>
2026-03-02 15:02:14 +0400

    Add sealed ItemUpsertDto record for item upsert operations

---
+++ Employee_Offboarding.Application/DTOs/Items/ItemUpsertDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_Offboarding.Application.DTOs.Items
+{
+    public sealed record ItemUpsertDto
+    (
+    int? Id,
+    int DepartmentId,
+    string Name,
+    bool IsTextRequired,
+    bool IsActive
+        );
+}
